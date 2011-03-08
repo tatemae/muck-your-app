792b15e7d4ae6c63ee98603001821e49892fdc73
Justin Ball <justinball@gmail.com>
2011-03-07 21:37:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muckyourapp::Application.config.secret_token = '71f54a2f2d8490cf2f44f26a83ecc3ffd778e6d4e05375ac77d1faf60cba13997d4f9e4f698a1c6c7a73b75a96a6a5fef0059fcbc0a9a7b09da69ee101c50f9e'
